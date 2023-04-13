0f8d1679df61b1d8431d700f27d4777a097d4423
Hamada-Gado <ahmedgado0100@gmail.com>
2023-04-13 21:13:20 +0200

    update comments again

--- Lists/p1_06.pl
+++ Lists/p1_06.pl
@@ -1,16 +1,17 @@
 % 1.06 (*) Find out whether a list is a palindrome.
 
-% palindrome(L) :- checks if list L is a palindrome
-%       (list)
-
-% palindrome(L1, L2, X) :- X is the first element in L1 and L2 is the rest of list L1 repeat until last element then compare them and do it again with L2.
-%       (list, list, element)
-
 % Note: this was solved without using built-in predict reverse
 % so, alternative solution is: palindrome(L) :- reverse(L,L). -_-
 
+% palindrome(L) :- checks if list L is a palindrome
+%       (list)
+
 palindrome([]).
 palindrome([H|T]) :- palindrome(T, [], H), !.
+
+% palindrome(L1, L2, X) :- X is the first element in L1 and L2 is the rest of list L1 
+% repeat until last element then compare them and do it again with L2.
+%       (list, list, element)
 
 palindrome([X], [], X) .
 palindrome([], [], _).
